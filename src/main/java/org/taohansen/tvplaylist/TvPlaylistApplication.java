01928aded41826912e1ff3e0d5d6c64509b87672
Tao Hansen <taohansens@gmail.com>
2024-03-15 00:07:43 -0300

    generate m3u. cors. front.

--- src/main/java/org/taohansen/tvplaylist/TvPlaylistApplication.java
+++ src/main/java/org/taohansen/tvplaylist/TvPlaylistApplication.java
@@ -2,6 +2,9 @@ package org.taohansen.tvplaylist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TvPlaylistApplication {
@@ -10,4 +13,16 @@ public class TvPlaylistApplication {
         SpringApplication.run(TvPlaylistApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowCredentials(true)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
